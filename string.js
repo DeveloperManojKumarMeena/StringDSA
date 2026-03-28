3de17d1024df71ad2d02a60a8945204229679948
DeveloperManojKumarMeena <manojkumarmeena1996@outlook.com>
2026-03-28 18:04:27 +0530

    regexi = /AdaLah/i

--- string.js
+++ string.js
@@ -1,4 +1,17 @@
+/*
 let s = 'A';
 let E = '😊';
 console.log('s length is '+ s.length)
 console.log('E length is '+ E.length)
+*/
+
+/*
+let s = "😊";
+
+console.log(s.charAt(0)); // half character 😬
+console.log([...s][0]);        // same half
+*/
+
+let str = 'WikiBuku adalah salah satu proyek Wikimedia yang dimulai sejak 10 Juli 2003. Sejak itu, para sukarelawan penulis telah menulis sekitar 5.542 halaman dan ratusan buku.'
+console.log(str.replace(/AdaLah/i,'Apple'))
+console.log(str.replace('adalah','Apple'))
