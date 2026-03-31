e4080bb93022558e8b7a8e79b905eff0fc71bfa5
DeveloperManojKumarMeena <manojkumarmeena1996@outlook.com>
2026-03-31 15:42:44 +0530

    array creation using Array.from with a mapping function to fill it with asterisks.

--- prototype.js
+++ prototype.js
@@ -1,6 +1,11 @@
+/*
 function manoj(num){
     return 5*num
 }
 manoj.age=28;
 console.log(`here we call function  ${manoj(4)}`);
 console.log(`here we show function object like behavior ${manoj.age}`)
+*/
+
+let arr = Array.from({length:3},()=>{Array(4).fill('*')})
+console.log(arr)
