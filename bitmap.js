32dcbdb46a29de6a39e110f3d72aecca5ac86608
DeveloperManojKumarMeena <manojkumarmeena1996@outlook.com>
2026-04-17 10:20:41 +0530

    bitmap is a simple program that counts the frequency of each character in a given string and prints the characters along with their frequencies. It uses the `prompt-sync` library to get user input from the command line. The program initializes an array `freqArr` of size 123 (to cover all ASCII characters) with zeros, then iterates through each character in the input string, converts it to its ASCII code using `charCodeAt`, and increments the corresponding index in `freqArr`. Finally, it iterates through `freqArr` and prints the characters that have a frequency greater than zero.

---
+++ bitmap.js
@@ -0,0 +1,14 @@
+let prompt = require('prompt-sync')();
+let s = prompt("Enter a string")
+let freqArr = new Array(123).fill(0);
+
+for(let i=0;i<s.length;i++){
+    let ascii = s.charCodeAt(i);
+    freqArr[ascii] = freqArr[ascii] + 1;
+}
+
+for(let i = 0; i<freqArr.length;i++){
+    if(freqArr[i]>0){
+        console.log(String.fromCharCode(i) + ' -> ' + freqArr[i]);
+    }
+}
