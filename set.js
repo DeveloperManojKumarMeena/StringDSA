dd6eaea988ff3a96619088e4dd45aa4f69dc5862
DeveloperManojKumarMeena <manojkumarmeena1996@outlook.com>
2026-04-18 07:04:09 +0530

    jewels & stones problem implementation

--- set.js
+++ set.js
@@ -1,8 +1,10 @@
+let prompt = require('prompt-sync')();
 /* Check if the pangram or not 
 :Pangram is a sentence where every letter of the english alphaabet
  appears at least once */
 
- let prompt = require('prompt-sync')();
+ /*
+ 
  let s = prompt('Please enter a string for check Pangram or Not.  ')
  let set = new Set();
 
@@ -12,3 +14,19 @@
  }
  if(set.size===26)console.log("String is Pangram")
     else console.log('Not Panagram')
+ */
+
+ //Jewels & Stones Problem
+let jewels = prompt('Enter Jewles value  ');
+let stones = prompt('Enter stones string  ');
+let set = new Set();
+let count = 0;
+
+for(let i=0;i<jewels.length;i++){
+    set.add(jewels.charAt(i))
+}
+for(let i=0;i<stones.length;i++){
+    let ch = stones.charAt(i);
+    if(set.has(ch)) count++
+}
+console.log(count)
