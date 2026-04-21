26620ab0f25d7676d25a40c7282dbc6291a23576
DeveloperManojKumarMeena <manojkumarmeena1996@outlook.com>
2026-04-21 07:25:46 +0530

    some important changes in map.js

--- map.js
+++ map.js
@@ -1,4 +1,23 @@
-let map = new Map();
+// let map = new Map();
 
-map.set('Name','Manoj Kumar Meena')
-console.log(map)
+// map.set('Name','Manoj Kumar Meena')
+// console.log({}+[])
+
+// function abc(){
+//     return{
+//         a:1
+//     }
+// }
+// console.log(abc())
+
+let arr = [10,10,20,30,4,40,30,20]
+
+function duplicate(){
+    return Math.max(...arr)
+}
+// console.log(duplicate())
+
+let arr2 = arr.reduce((acc,curr)=>{
+    return acc>curr?acc:curr
+})
+console.log(arr2)
